361045d5bbd412552caefcc2f22a0b04d5393a4b
github username <github email address>
2020-07-24 15:05:32 -0400

    db migration, seed and serializer creation

--- db/migrate/20200723211831_create_reviews.rb
+++ db/migrate/20200723211831_create_reviews.rb
@@ -2,10 +2,10 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.string :name
-      t.string :rating
-      t.string :age
+      t.integer :rating
+      t.integer :age
       t.string :comment
-      t.string :product_id
+      t.integer :product_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,30 @@
 #
 # Examples:
 #
+@brands=["Glossier","Alleyoop","Sephora","Cult Beauty","Clinique","Kate Somerville","Sobel","Origins","Murad","SkinMedica"]
+@descriptions=["Use at night","Everyday Use","New Formula crafted from professionals","Wildly tested","Made of beneficial ingredients","Great option for sensitive skin","Use daily and work up to twice a week","Popular Formula"]
+@comment=["Love this product", "Didn't Work for Me", "Great!","This was okay.","Not worth the price"]
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do 
+    product=Product.create(
+        name: Faker::Commerce.product_name,
+        brand: @brands[rand(@brands.length)],
+        category: Faker::Commerce.department(max: 1, fixed_amount: true),
+        description: @descriptions[rand(@descriptions.length)],
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTt-moY8D6NVGDiMeLYP-eqCWiGZ6CSJl8cCO3ZwlxeDzBaFhadjoCK97sg5N88espq5zVJaq3Z&usqp=CAc",
+    )
+end
+
+10.times do 
+    review=Review.create(
+        name: Faker::Name.name,
+        rating: rand(1..5),
+        age: rand(15..60),
+        comment: @comment[rand(@comment.length)],
+        product_id: rand(1..10)
+    )
+end
+
+
+
