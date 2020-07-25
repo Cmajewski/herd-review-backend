2133e6ae8f19b16dd711e4e33fd2ab2aec0f81a5
github username <github email address>
2020-07-24 23:40:42 -0400

    likes migration

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :brand, :category, :description, :image_url
+  attributes :id, :name, :brand, :category, :likes, :description, :image_url
   has_many :reviews
 end
